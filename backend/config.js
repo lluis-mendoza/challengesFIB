98b4990ac3781d93b2cf007e95e7e97e42a002ea
lluis.mendoza <@lluis.mendoza>
2021-05-16 00:14:04 +0200

    heroku deploy

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'port': 5000,
+    'port': process.env.PORT||5000,
     'database': 'mongodb+srv://lluis:achanta1030@cluster0.1dbi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     'secret': 'HdZZVPAjKeryfk9NKKfziLa1'
 };
