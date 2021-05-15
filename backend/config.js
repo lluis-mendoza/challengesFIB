3fdcc13e28af21d2911057d2646d5e1434922751
lluis.mendoza <@lluis.mendoza>
2021-05-15 23:51:43 +0200

    bug fixed

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'port': process.env.PORT||3006,
+    'port': 5000,
     'database': 'mongodb+srv://lluis:achanta1030@cluster0.1dbi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     'secret': 'HdZZVPAjKeryfk9NKKfziLa1'
 };
